dbc7c39a1d3199fd3f4cb394e7a38168c4294d4a
David Trannam <trannamd@oregonstate.edu>
2022-05-08 14:18:29 -0700

    created user sample data

--- Frontend/monch/src/App.js
+++ Frontend/monch/src/App.js
@@ -1,6 +1,14 @@
+import React, {useState} from 'react';
+import { sample_users } from "./sample_data/users"
+
 function App() {
+  const [users, setUsers] = useState(sample_users)
+
   return (
-    <h1>Testing</h1>
+    <div>
+    <h1>User Table</h1>
+    <button onClick={() => console.log(users)}>Click me for testing</button>
+    </div>
   );
 }
 
