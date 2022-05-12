a4ba9e0daf06d99b947a41216d60e9cefee811c9
Kevin <71728686+kevinluk1@users.noreply.github.com>
2022-05-12 02:19:09 -0700

    Add CouponRow.jsx, add Coupon.js

---
+++ Frontend/monch/src/routes/Coupons.js
@@ -0,0 +1,4 @@
+import React, {useState} from 'react';
+import NavBar from "../components/NavBar";
+import {sample_categories} from "../sample_data/categories"
+import CategoryRow from "../components/CategoryRow";
